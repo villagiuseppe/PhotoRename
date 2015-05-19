d843f9f69f05d726b403ea3622104f9948f0e80f
g.villa <giuseppe.villa@gmail.com>
2015-05-19 13:25:33 +0200

    IMP - Set Framework 2.0

--- Program.cs
+++ Program.cs
@@ -5,10 +5,9 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+
 
 
 namespace PhotoRename
